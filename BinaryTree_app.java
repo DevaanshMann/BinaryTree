66fe46f18125d926b5aa2b784ff399d2555e00cb
Devaansh Mann <devaanshmann5@gmail.com>
2024-03-18 17:08:36 -0700

    first commit

---
+++ BinaryTree_app.java
@@ -0,0 +1,18 @@
+public class BinaryTree_app {
+    public static void main(String[] args) {
+        BinaryTree C = new BinaryTree();
+        C.Insert(10); // this is the first value so it's the root
+        C.Insert(12);
+        C.Insert(8);
+        C.Insert(9);
+        C.Insert(13);
+        C.Insert(6);
+        C.Insert(14);
+        C.Insert(7);
+        C.DisplayTree(); // this displays the tree in text graphics
+
+        while (true);
+        
+    }
+        
+}
